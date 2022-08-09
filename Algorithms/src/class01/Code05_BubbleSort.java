8f9d6dc15865ee7e9c0b8ce2ac7db50387a29fd1
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-08-09 23:28:46 +0800

    update Code05_BubbleSort

--- Algorithms/src/class01/Code05_BubbleSort.java
+++ Algorithms/src/class01/Code05_BubbleSort.java
@@ -14,16 +14,16 @@ public class Code05_BubbleSort {
             return;
         }
         int N = arr.length;
-        for (int end = N - 1; end >= 0; end --) {
+        for (int end = N - 1; end >= 0; end--) {
             for (int second = 1; second <= end; second++) {
                 if (arr[second - 1] > arr[second]) {
-                    swap(arr,second - 1, second);
+                    swap(arr, second - 1, second);
                 }
             }
         }
     }
 
-    public static void insertSort(int[] arr) {
+    public static void insertSort1(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
@@ -37,6 +37,18 @@ public class Code05_BubbleSort {
         }
     }
 
+    public static void insertSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        for (int end = 1; end < N; end++) {
+            for (int pre = end - 1; pre >= 0 && arr[pre] > arr[pre + 1]; pre--) {
+                swap(arr, pre, pre + 1);
+            }
+        }
+    }
+
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -45,9 +57,9 @@ public class Code05_BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {7, 1, 3, 5, 1, 6, 8, 1, 3, 5, 7,5 ,6};
+        int[] arr = {7, 1, 3, 5, 1, 6, 8, 1, 3, 5, 7, 5, 6};
         printArray(arr);
-        insertSort(arr);
+        insertSort2(arr);
         printArray(arr);
     }
 }
