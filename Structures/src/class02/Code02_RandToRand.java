9d0151a3b81d7d0c59cfcdf877f92fb0607d1b8f
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-08-12 00:18:57 +0800

    update Code02_RandToRand_2

--- Structures/src/class02/Code02_RandToRand.java
+++ Structures/src/class02/Code02_RandToRand.java
@@ -27,6 +27,56 @@ public class Code02_RandToRand {
         System.out.println((double) count / (double) testTimes);
         System.out.println((double) 5 / (double) 8);
 
+        int K = 9;
+        // [0,K) -> [0,8]
 
+        int[] counts = new int[9];
+        for (int i = 0; i < testTimes; i++) {
+            int ans = (int) (Math.random() * K); // [0,K-1]
+            counts[ans]++;
+        }
+        for (int i = 0; i < K; i++) {
+            System.out.println(i + "这个数，出现了 " + counts[i] + " 次");
+        }
+
+        System.out.println("=========");
+
+        count = 0;
+        double x = 0.17;
+        for (int i = 0; i < testTimes; i++) {
+            if (xToXPower2Max() < x) {
+                count++;
+            }
+        }
+        System.out.println((double) count / (double) testTimes);
+        System.out.println(Math.pow(x, 2));
+
+        System.out.println("=========");
+
+        count = 0;
+        double y = 0.17;
+        for (int i = 0; i < testTimes; i++) {
+            if (xToXPower2Min() < y) {
+                count++;
+            }
+        }
+        System.out.println((double) count / (double) testTimes);
+        System.out.println((double) 1 - Math.pow((double) 1 - y, 2));
+
+        System.out.println("==========");
+
+
+    }
+
+    // 返回[0,1)的一个小数
+    // 任意的x，x属于[0,1)，[0,x)范围上的数出现概率由原来的x调整成x平方
+    public static double xToXPower2Max() {
+        return Math.max(Math.random(), Math.random());
+    }
+
+    // 返回[0,1)的一个小数
+    // 任意的x，x属于[0,1)，[0,x)范围上的数出现概率由原来的x调整成x平方
+    public static double xToXPower2Min() {
+        return Math.min(Math.random(), Math.random());
     }
 }
