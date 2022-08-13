be9b59624a4f239401e0862339858c887d8817d5
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-08-14 00:12:09 +0800

    update Code02_RandToRand_2

--- Structures/src/class02/Code02_RandToRand.java
+++ Structures/src/class02/Code02_RandToRand.java
@@ -64,7 +64,24 @@ public class Code02_RandToRand {
         System.out.println((double) 1 - Math.pow((double) 1 - y, 2));
 
         System.out.println("==========");
+        count = 0;
+        for (int i = 0; i < testTimes; i++) {
+            if (f2() == 0) {
+                count++;
+            }
+        }
+        System.out.println((double) count / (double) testTimes);
 
+        System.out.println("==========");
+
+        counts = new int[8];
+        for (int i = 0; i < testTimes; i++) {
+            int num = g();
+            counts[num]++;
+        }
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i + "这个数，出现了 " + counts[i] + " 次");
+        }
 
     }
 
@@ -78,5 +95,38 @@ public class Code02_RandToRand {
     // 任意的x，x属于[0,1)，[0,x)范围上的数出现概率由原来的x调整成x平方
     public static double xToXPower2Min() {
         return Math.min(Math.random(), Math.random());
+    }
+
+    // lib里的，不能改！
+    public static int f1() {
+        return (int) (Math.random() * 5) + 1;
+    }
+
+    // 随机机制，只能用f1,
+    // 等概率返回0和1
+    public static int f2() {
+        int ans = 0;
+        do {
+            ans = f1();
+        } while (ans == 3);
+        return ans < 3 ? 0 : 1;
+    }
+
+    // 得到000 ~ 111 做到等概率 0 ~ 7等概率返回一个
+    public static int f3() {
+        return (f2() << 2) + (f2() << 1) + f2();
+    }
+
+    // 0 ~ 6等概率返回一个
+    public static int f4() {
+        int ans = 0;
+        do {
+            ans = f3();
+        } while (ans == 7);
+        return ans;
+    }
+
+    public static int g() {
+        return f4() + 1;
     }
 }
