2211b7d60ae45d250a8f2003e836c7fa246db422
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-08-28 22:35:41 +0800

    add Code03_BitAddMinusMultiDiv.java

---
+++ arithmetic/src/class05/Code03_BitAddMinusMultiDiv.java
@@ -0,0 +1,35 @@
+package class05;
+
+// 测试链接：https://leetcode.com/problems/divide-two-integers
+public class Code03_BitAddMinusMultiDiv {
+
+    public static int add(int a, int b) {
+        int sum = a;
+        while (b != 0) {
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
+    }
+
+    public static int negNum(int n) {
+        return add(~n, 1);
+    }
+
+    public static int minus(int a, int b) {
+        return add(a, negNum(b));
+    }
+
+    public static int multi(int a, int b) {
+        int res = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                res = add(res, a);
+            }
+            a <<= 1;
+            b >>>= 1;
+        }
+        return res;
+    }
+}
