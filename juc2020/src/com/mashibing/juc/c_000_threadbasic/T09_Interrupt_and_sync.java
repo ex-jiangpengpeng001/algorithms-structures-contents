d0a9a00634d4e544ee2ead01b520eebc2fd14fd1
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-08-29 23:23:00 +0800

    add T09_Interrupt_and_sync.java

---
+++ juc2020/src/com/mashibing/juc/c_000_threadbasic/T09_Interrupt_and_sync.java
@@ -0,0 +1,36 @@
+package com.mashibing.juc.c_000_threadbasic;
+
+import com.mashibing.util.SleepHelper;
+
+/**
+ * interrupt与sleep() wait() join()
+ */
+public class T09_Interrupt_and_sync {
+
+    private static Object o = new Object();
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            synchronized (o) {
+                SleepHelper.sleepSeconds(10);
+            }
+        });
+
+        t.start();
+
+        SleepHelper.sleepSeconds(1);
+
+        Thread t2 = new Thread(()->{
+            synchronized (o) {
+
+            }
+            System.out.println("t2 end!");
+        });
+
+        t2.start();
+
+        SleepHelper.sleepSeconds(1);
+
+        t2.interrupt();
+    }
+}
