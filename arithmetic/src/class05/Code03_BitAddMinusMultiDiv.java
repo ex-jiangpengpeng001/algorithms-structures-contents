44ac00dfa4b70caf378d661aebaf3793582374ce
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-09-02 22:55:52 +0800

    add Code03_BitAddMinusMultiDiv.java

--- arithmetic/src/class05/Code03_BitAddMinusMultiDiv.java
+++ arithmetic/src/class05/Code03_BitAddMinusMultiDiv.java
@@ -32,4 +32,39 @@ public class Code03_BitAddMinusMultiDiv {
         }
         return res;
     }
+
+    public static boolean isNeg(int n) {
+        return n < 0;
+    }
+
+    public static int div(int a, int b) {
+        int x = isNeg(a) ? negNum(a) : a;
+        int y = isNeg(b) ? negNum(b) : b;
+        int res = 0;
+        for (int i = 30; i >= 0; i = minus(i, 1)) {
+            if ((x >> i) >= y) {
+                res |= (1 << i);
+                x = minus(x, y << i);
+            }
+        }
+        return isNeg(a) ^ isNeg(b) ? negNum(res) : res;
+    }
+
+    public static int divide(int a, int b) {
+        if (a == Integer.MIN_VALUE && b == Integer.MIN_VALUE) {
+            return 1;
+        } else if (b == Integer.MIN_VALUE) {
+            return 0;
+        } else if (a == Integer.MIN_VALUE) {
+            if (b == negNum(1)) {
+                return Integer.MAX_VALUE;
+            } else {
+                int c = div(add(a, 1), b);
+                return add(c, div(minus(a, multi(c, b)), b));
+            }
+        } else {
+            return div(a, b);
+        }
+    }
+
 }
