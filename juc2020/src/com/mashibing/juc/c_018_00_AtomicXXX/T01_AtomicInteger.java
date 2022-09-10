0eb3b0b6885f66687db8cfa092ed05f5fc75f670
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-09-10 18:23:45 +0800

    add c_018_00_AtomicXXX

---
+++ juc2020/src/com/mashibing/juc/c_018_00_AtomicXXX/T01_AtomicInteger.java
@@ -0,0 +1,39 @@
+package com.mashibing.juc.c_018_00_AtomicXXX;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class T01_AtomicInteger {
+
+    AtomicInteger count = new AtomicInteger(0);
+
+    /*synchronized*/ void m() {
+        for (int i = 0; i < 10000; i++)
+            //if count1.get() < 1000
+            count.incrementAndGet(); //count1++
+    }
+
+    public static void main(String[] args) {
+        T01_AtomicInteger t = new T01_AtomicInteger();
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            threads.add(new Thread(t::m, "thread-" + i));
+        }
+
+        threads.forEach((o) -> o.start());
+
+        threads.forEach((o) -> {
+            try {
+                o.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println(t.count);
+    }
+
+}
