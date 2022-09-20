84b6489d191c17ff8200594e6b2cf8c5a203f11c
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-09-20 23:12:22 +0800

    add DesignPatterns

---
+++ DesignPatterns/src/com/mashibing/dp/singleton/Mgr06.java
@@ -0,0 +1,36 @@
+package com.mashibing.dp.singleton;
+
+public class Mgr06 {
+    private static volatile Mgr06 INSTANCE;
+
+    private Mgr06() {};
+
+    public static Mgr06 getInstance() {
+        if (INSTANCE == null) {
+            // 双重检查
+            synchronized(Mgr06.class) {
+                if (INSTANCE == null) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    INSTANCE = new Mgr06();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public void m() {
+        System.out.println("m");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->
+                    System.out.println(Mgr06.getInstance().hashCode())
+            ).start();
+        }
+    }
+}
