d78e68c1d2d3dec4033ad9a18661982d82042268
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-10-30 17:52:15 +0800

    update Mgr01.java

--- DesignPatterns/src/com/mashibing/dp/singleton/Mgr01.java
+++ DesignPatterns/src/com/mashibing/dp/singleton/Mgr01.java
@@ -1,5 +1,13 @@
 package com.mashibing.dp.singleton;
 
+/**
+ * 饿汉式
+ * 类加载到内存后，就实例化一个单例，JVM保证线程安全
+ * 简单实用，推荐使用！
+ * 唯一缺点：不管用到与否，类加载时就完成实例化
+ * Class.forName("")
+ * （话说你不用到，你装载它干啥）
+ */
 public class Mgr01 {
     private static final Mgr01 INSTANCE = new Mgr01();
 
