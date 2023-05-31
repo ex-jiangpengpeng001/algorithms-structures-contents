62996654fc2f279db1882038db411723c22f8b21
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2023-05-31 23:20:34 +0800

    mod class01

--- juc2020/src/com/mashibing/JustTest.java
+++ juc2020/src/com/mashibing/JustTest.java
@@ -1,7 +1,10 @@
 package com.mashibing;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class JustTest {
     public static void main(String[] args) {
         Object o = new Object();
+
     }
 }
